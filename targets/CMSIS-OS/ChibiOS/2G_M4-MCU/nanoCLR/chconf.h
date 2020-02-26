826db9203a308df543594a3b8244c12590bae66e
Martin Kuhn <martin.kuhn@csa.ch>
2020-02-26 17:01:36 +0100

    [os] disable debug checks

--- targets/CMSIS-OS/ChibiOS/2G_M4-MCU/nanoCLR/chconf.h
+++ targets/CMSIS-OS/ChibiOS/2G_M4-MCU/nanoCLR/chconf.h
@@ -407,7 +407,7 @@
  *
  * @note    The default is @p FALSE.
  */
-#define CH_DBG_SYSTEM_STATE_CHECK           TRUE
+#define CH_DBG_SYSTEM_STATE_CHECK           FALSE
 
 /**
  * @brief   Debug option, parameters checks.
@@ -416,7 +416,7 @@
  *
  * @note    The default is @p FALSE.
  */
-#define CH_DBG_ENABLE_CHECKS                TRUE
+#define CH_DBG_ENABLE_CHECKS                FALSE
 
 /**
  * @brief   Debug option, consistency checks.
@@ -426,7 +426,7 @@
  *
  * @note    The default is @p FALSE.
  */
-#define CH_DBG_ENABLE_ASSERTS               TRUE
+#define CH_DBG_ENABLE_ASSERTS               FALSE
 
 /**
  * @brief   Debug option, trace buffer.
