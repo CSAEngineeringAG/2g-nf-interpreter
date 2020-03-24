59b186231e8e37a112d4a3e4532cbffcc90fbbd8
Martin Kuhn <martin.kuhn@csa.ch>
2020-03-24 11:57:28 +0100

    [build] overwrite clib realloc()

--- targets/CMSIS-OS/ChibiOS/common/platform_heap.c
+++ targets/CMSIS-OS/ChibiOS/common/platform_heap.c
@@ -6,6 +6,7 @@
 #include <ch.h>
 #include <stdlib.h>
 #include <string.h>
+#include <nanoHAL_v2.h>
 
 void* platform_malloc(size_t size) {
     return chHeapAlloc(NULL, size);
@@ -34,4 +35,21 @@ void* calloc (size_t num, size_t size) {
     void* p = platform_malloc(num*size);
     memset(p,0,num*size);
     return p;
+}
+
+void *realloc( void *ptr, size_t new_size ){
+	if (ptr == NULL) {
+		// pointer is null --> allocate memory block
+		return platform_malloc(new_size);
+	} else {
+		if (new_size == 0) {
+			// new size is 0 --> free memory
+			platform_free(ptr);
+			return NULL;
+		} else {
+			// pointer is not NULL and new size is not 0 --> allocate new memory block
+			ASSERT(false);
+			return ptr;
+		}
+	}
 }
